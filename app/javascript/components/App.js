6e2ab218c5de7907d5d6d68310934c09e957a7b3
MMORONTO <t2wycemo@gmail.com>
2021-06-11 14:55:18 -0400

    adjust rails routs to resolve rendering issue

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,21 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
 
-const App = () => {
-    return <div>
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/" component={About} />
-            <Route exact path="/" component={Contact} />
-        </Switch>
-    </div>
+class App extends React.Component {
+    render () {
+        return (
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/" component={About} />
+                    <Route exact path="/" component={Contact} />
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default App
