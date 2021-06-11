147b9216a986459dc5c8388abdec3431aea7daff
MMORONTO <t2wycemo@gmail.com>
2021-06-11 05:58:17 -0400

    refactor react code in App.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 
-class App extends Component {
-    render() {
-        return (
-            <div>This is my newest app!</div>
-        )
-    }
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>This is my newest app!</div>
+//         )
+//     }
+// }
+
+const App = () => {
+    return <div>This is flippin' Awesome!!</div>
 }
 
 export default App
