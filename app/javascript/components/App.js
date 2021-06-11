fcd4ee0ed7e2a3ad0e398dbb346569fb99dc66f2
MMORONTO <t2wycemo@gmail.com>
2021-06-11 14:27:53 -0400

    import new compnents into react App

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
 
 const App = () => {
     return <div>
