3eccee1d83deb2ba650fd2043ce7c31ce6cbb086
MMORONTO <t2wycemo@gmail.com>
2021-06-11 05:43:16 -0400

    rename default react folder & import new component from components folder to renamed js folder

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react'
 class App extends Component {
     render() {
         return (
-            <div>THis is my newest app!</div>
+            <div>This is my newest app!</div>
         )
     }
 }
+
+export default App
