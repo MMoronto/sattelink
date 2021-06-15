48165b6cfceaac84c75c14831ded2d13854972ee
MMORONTO <t2wycemo@gmail.com>
2021-06-15 14:21:08 -0400

    class component example

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
-class Home extends React.Component {
-    render(){
-        return(
-            <div>
-                <h1>Hello, this is the home page.</h1>
-            </div>
-        )
-    }
+// class Home extends React.Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h1>Hello, this is the home page.</h1>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Home
+
+function Home(props) {
+    return (
+        <div id="root">
+            <h1>Hello, this is the{this.props} page.</h1>
+        </div>        
+    );
 }
 
-export default Home
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+)
