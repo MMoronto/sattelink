033d160c5787f911a7c0a0ac0aad93939fa44728
MMORONTO <t2wycemo@gmail.com>
2021-06-19 12:43:25 -0400

    seed database with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,51 +9,51 @@ Item.create([
     {
         name: "Compact GNSS Active Patch Antenna",
         image_url: "https://sattelink.s3.amazonaws.com/Compact-GNSS-Active-Patch-Antenna.png",
-        price: "$2,400"
+        price: "$2400"
     },
     {
         name: "Attitude actuators",
         image_url: "https://sattelink.s3.amazonaws.com/Attitude-actuators.png",
-        price: "$6,300"
+        price: "$6300.50"
     },
     {
         name: "Attitude Sensors",
         image_url: "https://sattelink.s3.amazonaws.com/Attitude-sensors.png",
-        price: "$2,400"
+        price: "$2400.45"
     },
     {
         name: "Cameras & Payloads",
         image_url: "https://sattelink.s3.amazonaws.com/Cameras-&-Payloads.png",
-        price: "$2,400"
+        price: "$2400.00"
     },
     {
         name: "Command & Data Handling",
         image_url: "https://sattelink.s3.amazonaws.com/Command-&-Data-Handling.png",
-        price: "$16,500"
+        price: "$16500"
     },
     {
         name: "Communication Systems",
         image_url: "https://sattelink.s3.amazonaws.com/Communication-Systems.png",
-        price: "$6,600"
+        price: "$6600"
     },
     {
         name: "CubeSat Kits & Buses",
         image_url: "https://sattelink.s3.amazonaws.com/CubeSat-Kits-&-Buses.png",
-        price: "$59,850"
+        price: "$59850"
     },
     {
         name: "CubeSat Structures",
         image_url: "https://sattelink.s3.amazonaws.com/CubeSat-Structures.png",
-        price: "$2,950"
+        price: "$2950"
     },
     {
         name: "Ground Stations",
         image_url: "https://sattelink.s3.amazonaws.com/Ground-Stations.png",
-        price: "$14,500"
+        price: "$14500"
     },
     {
         name: "Launch Adapters",
         image_url: "https://sattelink.s3.amazonaws.com/Launch-Adapters.png",
-        price: "$2,950"
+        price: "$2950"
     }                
 ])
