cc9e0a9e40b0bcfbd4b7ab96b95e1e08d85669c6
MMORONTO <t2wycemo@gmail.com>
2021-06-22 22:42:06 -0400

    add index method to controller. Render the data as JSON using ItemSerializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,7 +1,11 @@
 module Api
     module V1
         class ItemsController < ApplicationController
-            
+            def index
+                items = Item.all
+
+                render json: ItemSerializer.new(items).serialized_json
+            end
         end
     end
 end
