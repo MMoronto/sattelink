34bd79ddc904287ab665ea0a5bb607e141753fd1
MMORONTO <t2wycemo@gmail.com>
2021-06-24 11:05:42 -0400

    create whitelist using strong params

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,7 +1,12 @@
 module Api
     module V1
         class ReviewsController < ApplicationController
-            
+
+            private
+
+            def review_params
+                params.require(:review).permit(:title, :description, :score, item_id)
+            end
         end
     end
 end
