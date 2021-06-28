aff0b6fb9f4257da940488e3c53fe7e6c17c4a49
MMORONTO <t2wycemo@gmail.com>
2021-06-28 16:06:39 -0400

    setup homepage layout

--- app/javascript/components/Items/Items.js
+++ app/javascript/components/Items/Items.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Items = () => {
     const [items, setItems] = useState([])
-    
+
     return(
-        <div>This is the Items#index page for our app.</div>
+        <div className="home">
+            <div className="header">
+                <h1>Sattelink</h1>
+                <p>Number One webmarket place for CubeSats</p>
+            </div>
+            <div className="grid"></div>
+        </div>
     )
 }
 
