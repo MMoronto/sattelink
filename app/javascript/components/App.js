c93d13a04602e5506a22326f3df6a4b539b5e9a6
MMORONTO <t2wycemo@gmail.com>
2021-06-28 14:48:26 -0400

    revise item routes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
 
-class App extends React.Component {
-    render () {
-        return (
-            <div>
-                <Switch>
-                    <Route exact path="/" component={Items} />
-                    <Route exact path="/:slug" component="Item" />
-                    {/* <Route exact path="/contact" component={Contact} /> */}
-                </Switch>
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Items} />
+            <Route exact path="/:slug" component={Item} />
+        </Switch>
+    )
 }
 
 export default App
