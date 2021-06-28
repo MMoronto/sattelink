69f06945946df29a4045eece393f3e48a0641a6c
MMORONTO <t2wycemo@gmail.com>
2021-06-28 15:52:08 -0400

    Set default state in items component. set initial state as an empty array using hooks

--- app/javascript/components/Items/Items.js
+++ app/javascript/components/Items/Items.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const Items = () => {
+    const [items, setItems] = useState([])
+    
     return(
         <div>This is the Items#index page for our app.</div>
     )
