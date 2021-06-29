28d8bea97e47c4f9485e4a91089bdffb3cef5188
MMORONTO <t2wycemo@gmail.com>
2021-06-29 12:17:40 -0400

    add useEffect to items component for collecting items from our api

--- app/javascript/components/Items/Items.js
+++ app/javascript/components/Items/Items.js
@@ -1,13 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Items = () => {
     const [items, setItems] = useState([])
+
+    useEffect(()=> {
+        // Get all of our items from api
+        // Update items in our state
+    })
 
     return(
         <div className="home">
             <div className="header">
                 <h1>Sattelink</h1>
-                <p className="subheader">Number One webmarket place for CubeSats</p>
+                <p className="subheader">Number One webmarket for CubeSats</p>
             </div>
             <div className="grid">
                 ITEMS GRID GOES HERE
