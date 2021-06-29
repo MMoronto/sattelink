278d031489df36d3340736e2ea6c4c1beb75ee1c
MMORONTO <t2wycemo@gmail.com>
2021-06-29 07:21:55 -0400

    debug item/items & App components. Fix render issue

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Items from './Items/Items'
-import Items from './Item/Item'
+import Item from './Item/Item'
 
 const App = () => {
     return (
--- app/javascript/components/Items/Items.js
+++ app/javascript/components/Items/Items.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 const Items = () => {
     const [items, setItems] = useState([])
